a3c6a6cb7cfaa5075532b840c8c2387024783b4f
Reuven <retzion@gmail.com>
2020-05-11 23:07:52 -0600

    Log errors in webhook requests

--- src/api/webhooks.js
+++ src/api/webhooks.js
@@ -5,40 +5,46 @@ const { mongoConnect } = require('../mongo')
 // const bugCatcherApi = nodeBugCatcher(apiUrl)
 
 async function githubWebhook (request) {
-  const { body, headers } = request
-  const { "x-github-event": githubEvent } = headers
+  try {
 
-  if (!githubEvent || githubEvent !== 'push') return
-  else {
-    const { compare } = body
-    if (!compare) return
+    const { body, headers } = request
+    const { "x-github-event": githubEvent } = headers
 
-    // reponse expected by GitHub
-    const githubResponse = {"result":"ok"}
+    if (!githubEvent || githubEvent !== 'push') return
+    else {
+      const { compare } = body
+      if (!compare) return
 
-    // Look for a duplicate 
-    const findKey = { "requestBody.compare": compare }
-    const fn = async (db, promise) => {
-      const githubScansCollection = db.collection('github_scans')
-      const savedScan = await githubScansCollection.find(findKey)
-        .toArray().catch(promise.reject)
-      
-      if (savedScan.length) promise.resolve(githubResponse)
-      else {
-        const saved = await githubScansCollection.updateOne(
-          findKey,
-          { $set: {
-            requestBody: body
-          }},
-          { upsert: true }
-        ).catch(promise.reject)
+      // reponse expected by GitHub
+      const githubResponse = {"result":"ok"}
 
-        if (saved) promise.resolve(githubResponse)
-        else promise.reject()
+      // Look for a duplicate 
+      const findKey = { "requestBody.compare": compare }
+      const fn = async (db, promise) => {
+        const githubScansCollection = db.collection('github_scans')
+        const savedScan = await githubScansCollection.find(findKey)
+          .toArray().catch(promise.reject)
+        
+        if (savedScan.length) promise.resolve(githubResponse)
+        else {
+          const saved = await githubScansCollection.updateOne(
+            findKey,
+            { $set: {
+              requestBody: body
+            }},
+            { upsert: true }
+          ).catch(promise.reject)
+
+          if (saved) promise.resolve(githubResponse)
+          else promise.reject()
+        }
       }
+      return mongoConnect(fn)
     }
-    return mongoConnect(fn)
-
+  }
+  catch(err) {
+    console.error(err)
+    return ({ error: err })
   }
 }
 
